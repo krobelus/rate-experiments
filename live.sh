d3a6b658aa46e686999733d4c1c087e02ffcd454
Johannes Altmanninger <aclopte@gmail.com>
2019-07-18 15:50:28 +0200

    rename

--- live.sh
+++ live.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ls README.md poster/INF_Poster.tex plots.ipynb | entr pipenv run make README.pdf README.markdown poster
+ls README.md poster/poster.tex plots.ipynb | entr pipenv run make README.pdf README.markdown poster
