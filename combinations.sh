002c5ff734bd54447a447b2f694607850980fa5c
Johannes Altmanninger <aclopte@gmail.com>
2019-08-17 22:06:22 +0200

    update

--- combinations.sh
+++ combinations.sh
@@ -8,6 +8,7 @@ while read instance
 do
 	for solver in $solvers
 	do
-		printf "%s/%s\n" $instance $solver
+		# only already solved instances
+		test -f benchmarks/"$instance"/"$solver"/proof.out.zst && printf "%s/%s\n" $instance $solver
 	done
 done < instances.txt
--- run-instance-solver-combination.sh
+++ run-instance-solver-combination.sh
@@ -7,6 +7,13 @@ checkers="$1"
 instance="${2%/*}"
 solver_with_config="${2#*/}"
 
+if [ -n "$3" ]; then
+	while true
+	do
+		DISCARD=1 ./run-checker.sh 'rate-dDISCARD' "$solver_with_config" "$instance"
+	done
+fi
+
 ./run-solver.sh "$solver_with_config" "$instance"
 for checker in $checkers
 do
