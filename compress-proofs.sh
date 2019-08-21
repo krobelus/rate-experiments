bafd5e871f8b8caf3bfc772073b20dcd120cd219
Johannes Altmanninger <aclopte@gmail.com>
2019-08-21 17:34:02 +0200

    update

--- compress-proofs.sh
+++ compress-proofs.sh
@@ -11,10 +11,10 @@ proof="$1"
 echo "$proof"
 pwd="$(dirname "$proof")"
 uncompressed="$(echo "$proof"|sed 's/.zst$//')"
-zstd -d "$proof" --force
-if ! tools/bin/is-binary-drat.pl "$uncompressed"; then
+zstdcat "$proof" | tools/bin/is-binary-drat.pl /dev/stdin || (
+	zstd -d "$proof" --force
 	tools/bin/drat2cdrat < "$uncompressed" > "$uncompressed".tmp
 	tools/bin/zstd -5 "$uncompressed".tmp -o "$proof" --force
-fi
+)
 rm -f "$uncompressed"
 
