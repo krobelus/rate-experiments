80d989c7846ec7d8fe82eb734f6170a798610ee8
Johannes Altmanninger <aclopte@gmail.com>
2019-08-21 16:49:58 +0200

    update

--- compress-proofs.sh
+++ compress-proofs.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+#
+# # run like this:
+# for proof in benchmarks/*/*/proof.out.zst
+# echo "$proof"
+# do | parallel ./compress-proofs.sh
 
 set -e -u
 
-for proof in benchmarks/*/*/proof.out.zst
-do
-	echo "$proof"
-	pwd="$(dirname "$proof")"
-	uncompressed="$(echo "$proof"|sed 's/.zst$//')"
-	zstd -d "$proof"
-	if ! tools/bin/is-binary-drat.pl "$uncompressed"; then
-		tools/bin/drat2cdrat < "$uncompressed" > "$uncompressed".tmp
-		tools/bin/zstd -5 "$uncompressed".tmp -o "$proof" --force
-	fi
-	rm -f "$uncompressed"
-done
+proof="$1"
+echo "$proof"
+pwd="$(dirname "$proof")"
+uncompressed="$(echo "$proof"|sed 's/.zst$//')"
+zstd -d "$proof"
+if ! tools/bin/is-binary-drat.pl "$uncompressed"; then
+	tools/bin/drat2cdrat < "$uncompressed" > "$uncompressed".tmp
+	tools/bin/zstd -5 "$uncompressed".tmp -o "$proof" --force
+fi
+rm -f "$uncompressed"
 
