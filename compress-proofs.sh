cf741a0dc117adb3c72a2650b94e7ded93e002b0
Johannes Altmanninger <aclopte@gmail.com>
2019-08-23 17:16:35 +0200

    fix

--- compress-proofs.sh
+++ compress-proofs.sh
@@ -3,18 +3,22 @@
 # # run like this:
 # for proof in benchmarks/*/*/proof.out.zst
 # echo "$proof"
-# do | parallel ./compress-proofs.sh
+# do |
+# parallel ./compress-proofs.sh \
+# --limit "df | sort -nk 2 | tail -n1 | perl -ae 'exit 1 if @F[3] < 70*(1<<20)'" \
 
 set -e -u
+
+zstd() {
+	tools/bin/zstd "$@"
+}
 
 proof="$1"
 echo "$proof"
 pwd="$(dirname "$proof")"
-uncompressed="$(echo "$proof"|sed 's/.zst$//')"
-tools/bin/zstd -dcf "$proof" | tools/bin/is-binary-drat.pl /dev/stdin || (
-	tools/bin/zstd -d "$proof" --force
-	tools/bin/drat2cdrat < "$uncompressed" > "$uncompressed".tmp
-	rm -f "$uncompressed"
-	tools/bin/zstd -5 "$uncompressed".tmp -o "$proof" --force --rm
+
+zstd -dcf "$proof" | tools/bin/is-binary-drat.pl /dev/stdin || (
+	zstd -dcf "$proof" --force | tools/bin/drat2cdrat | zstd - -o "$proof".new -5 --force
+	mv "$proof".new "$proof"
 )
 
