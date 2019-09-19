ec97f1a218cdad93c258aebc2ab4102efcf3f2e7
Johannes Altmanninger <aclopte@gmail.com>
2019-09-19 09:55:53 +0200

    update

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ set -eu
 
 ignored='thesis.pdf poster/poster.pdf thesis.tex t p'
 
-git commit -am "${1:-update}"||:
+git commit -am "${1:-update}"
 git filter-branch -f --prune-empty --index-filter "git rm -r --cached --ignore-unmatch $ignored" HEAD
 git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
 git reflog expire --expire=now --all
